4485f00dc5d74b31798ae4ea2923900bfb0c7e80
h594809 <111974742+h594809@users.noreply.github.com>
2022-11-16 20:34:14 +0100

    OBLIG3

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -5,46 +5,67 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	private Innlegg[] innleggstabell;
+	
+	private int nesteledig;
 
+	
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this(20);
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggstabell = new Innlegg[lengde];
+		
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteledig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggstabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		for(int i = 0; i < nesteledig; i++) {
+			if(innleggstabell[i].erLik(innlegg)) {
+				return i;
+			}	
+		}
+		return -1;		
+ 
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		return finnInnlegg(innlegg) != -1;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return nesteledig <= innleggstabell.length;
+		
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+     
+		if(!ledigPlass() || finnes(innlegg)) {
+			return false;
+		}
+		innleggstabell[nesteledig] = innlegg;
+		nesteledig++;
+		return true;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		var strr = new StringBuilder(Integer.toString(nesteledig));
+		for(Innlegg innlegg : innleggstabell) {
+			if(innlegg == null) {
+				break;
+			}
+			strr.append(innlegg.toString());
+		}
+		return strr.toString();
 	}
 
 	// valgfrie oppgaver nedenfor
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -8,8 +8,23 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
+	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jlab11/tests/";
+	
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		try {
+			var writer = new PrintWriter(MAPPE + filnavn);
+			
+			writer.println(samling.toString());
+			writer.close();
+			return true;
+		}
+		catch(FileNotFoundException E) {
+			System.out.println("finner ikkje fil");
+			
+		}
+		return false;
+ 
+		
 	}
 }
